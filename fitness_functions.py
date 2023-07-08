6f0aa5a1b316328436dadfb0fa08752ee90ceee7
Sven Farwig <Sven.farwig@rwth-aachen.de>
2023-07-08 02:49:34 +0200

    First versions for diversity fitness

--- fitness_functions.py
+++ fitness_functions.py
@@ -7,13 +7,36 @@ def diversity_func(population):
 
 # ToDo: fitness function for unconstrained case
 def fitness_func(ga_instance, solution, solution_idx):
-    pass
+    fitness = len(ga_instance.population) * len(solution)
+    for individual in ga_instance.population:
+        for i in range(len(solution)):
+            if solution[i] != individual[i]:
+                fitness = fitness + 1
+    return fitness
 
 
 # ToDo: fitness function for constrained case
-def fitness_func(ga_instance, solution, solution_idx):
-    pass
+def fitness_func_diversity(ga_instance, solution, solution_idx):
+    """
+    Calculates the diversity by reducing it for every node in the solutions vertex cover
+    """
+    fitness = len(ga_instance.population) * len(solution)
+    for individual in ga_instance.population:
+        for i in range(len(solution)):
+            if solution[i] == 1:
+                if individual[i] == 1:
+                    fitness = fitness - 1
+    print("fitness: ", end="")
+    print(fitness, end="")
+    print(" for ", end="")
+    print(solution)
+    return fitness
 
 
-def mvc_optimal_fitness(ga_instance, solution, solution_idx, n):
-    return n - np.sum(solution)
+def mvc_optimal_fitness(ga_instance, solution, solution_idx):
+    fitness = len(solution) - np.sum(solution)
+    #print("fitness: ", end="")
+    #print(fitness, end="")
+    #print(" for ", end="")
+    #print(solution)
+    return fitness
--- general_ea_settings.py
+++ general_ea_settings.py
@@ -1,14 +1,12 @@
-from fitness_functions import mvc_optimal_fitness
+from fitness_functions import mvc_optimal_fitness, fitness_func_diversity, fitness_func
 from initial_population import all_ones_pop
 from instance_generator import load_instance
 from operators import multi_node_swap
 
-NUM_GENERATIONS = 50
-GRAPH_INSTANCE = load_instance("instances/5_0.2_0.2.txt")
+NUM_GENERATIONS = 500
+GRAPH_INSTANCE = load_instance("instances/50_0.2_0.18.txt")
 NUM_GENES = len(GRAPH_INSTANCE)
-POPULATION_SIZE = 2
+POPULATION_SIZE = 3
 POPULATION_GENERATOR = all_ones_pop
 MUTATION_TYPE = lambda x, y: multi_node_swap(x, y, GRAPH_INSTANCE)
-FITNESS_FUNCTION = lambda x, y, z: mvc_optimal_fitness(x, y, z, NUM_GENES)
-
-print(GRAPH_INSTANCE)
+FITNESS_FUNCTION = mvc_optimal_fitness
