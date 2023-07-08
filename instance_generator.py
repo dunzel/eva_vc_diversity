c477c0100fa7aa7ca4f281cda6fd012c72656943
Sven Farwig <Sven.farwig@rwth-aachen.de>
2023-07-08 02:51:43 +0200

    fixed instance gerator for undirected graphs and generated new instances

--- instance_generator.py
+++ instance_generator.py
@@ -7,26 +7,28 @@ def generate_graph(n, delta):
     :param delta: density of the graph where delta=1 is a complete graph and delta=0 is an isolated graph
     :return:
     """
-    adjency_matrix = np.zeros((n, n))
+    adjacency_matrix = np.zeros((n, n))
     for i in range(n):
         for j in range(n):
             if i != j:
-                if np.random.rand() < delta:
-                    adjency_matrix[i][j] = 1
+                if i < j:
+                    if np.random.rand() < delta:
+                        adjacency_matrix[i][j] = 1
+                        adjacency_matrix[j][i] = 1
 
-    return adjency_matrix
+    return adjacency_matrix
 
 
-def save_instance(adjency_matrix, path):
+def save_instance(adjacency_matrix, path):
     """
-    :param adjency_matrix: adjency matrix of the graph
+    :param adjacency_matrix: adjency matrix of the graph
     :param path: path to save the instance
     """
     with open(path, "w") as file:
-        file.write(str(len(adjency_matrix)) + "\n")
-        for i in range(len(adjency_matrix)):
-            for j in range(len(adjency_matrix)):
-                if adjency_matrix[i][j] == 1:
+        file.write(str(len(adjacency_matrix)) + "\n")
+        for i in range(len(adjacency_matrix)):
+            for j in range(len(adjacency_matrix)):
+                if adjacency_matrix[i][j] == 1:
                     file.write(str(i) + " " + str(j) + "\n")
 
 
@@ -37,28 +39,29 @@ def load_instance(path):
     """
     with open(path, "r") as file:
         n = int(file.readline())
-        adjency_matrix = np.zeros((n, n))
+        adjacency_matrix = np.zeros((n, n))
         for line in file.readlines():
             i, j = line.split()
-            adjency_matrix[int(i)][int(j)] = 1
-            adjency_matrix[int(j)][int(i)] = 1  # undirected graph!
+            adjacency_matrix[int(i)][int(j)] = 1
+            adjacency_matrix[int(j)][int(i)] = 1  # undirected graph!
 
-    return adjency_matrix
+    return adjacency_matrix
 
 
-def calculate_delta(adjency_matrix):
+def calculate_delta(adjacency_matrix):
     """
-    :param adjency_matrix:
+    :param adjacency_matrix:
     :return: the density of the graph
     """
-    n = len(adjency_matrix)
+    n = len(adjacency_matrix)
     edges = 0
     for i in range(n):
         for j in range(n):
-            if adjency_matrix[i][j] == 1:
-                edges += 1
+            if i < j:
+                if adjacency_matrix[i][j] == 1:
+                    edges += 1
 
-    return edges / (n * (n - 1))
+    return edges / ((n * (n - 1)) / 2)
 
 
 def generator_test_samples():
@@ -67,8 +70,13 @@ def generator_test_samples():
     n \in {5, 10, 15}
     delta \in {0, 0.2, 0.5, 0.7, 1}
     """
-    for n in [5, 10, 15]:
-        for delta in [0, 0.2, 0.5, 0.7, 1]:
-            adjency_matrix = generate_graph(n, delta)
-            resulting_delta = round(calculate_delta(adjency_matrix), 2)
-            save_instance(adjency_matrix, "instances/" + str(n) + "_" + str(delta) + "_" + str(resulting_delta) + ".txt")
+    for n in [50]:
+        for delta in [0.2, 0.5]:
+            adjacency_matrix = generate_graph(n, delta)
+            resulting_delta = round(calculate_delta(adjacency_matrix), 2)
+            save_instance(adjacency_matrix,
+                          "instances/" + str(n) + "_" + str(delta) + "_" + str(resulting_delta) + ".txt")
+
+
+#generator_test_samples()
+
--- instance_renderer.py
+++ instance_renderer.py
@@ -5,41 +5,39 @@ from general_ea_settings import GRAPH_INSTANCE
 from instance_generator import load_instance
 
 
-def plain_graph(adjency_matrix):
+def plain_graph(adjacency_matrix):
     """
     basic graph renderer / without vertex cover
-    :param adjency_matrix: adjency matrix of the graph
+    :param adjacency_matrix: adjency matrix of the graph
     :return:
     """
     graph = nx.Graph()
-    for i in range(len(adjency_matrix)):
+    for i in range(len(adjacency_matrix)):
         graph.add_node(i)
-        for j in range(len(adjency_matrix)):
-            if adjency_matrix[i][j] == 1:
+        for j in range(len(adjacency_matrix)):
+            if adjacency_matrix[i][j] == 1:
                 graph.add_edge(i, j)
                 
     start_pos = nx.spring_layout(graph, seed=100)
     nx.draw(graph, with_labels=True, pos=start_pos)
     plt.show()
 
-plain_graph(GRAPH_INSTANCE)
-
 
 # 2. vertex cover renderer
 # should also render Vertex Cover somehow
 # colors nodes that are in the vertex cover
-def vertex_cover_graph(adjency_matrix, vertex_cover):
+def vertex_cover_graph(adjacency_matrix, vertex_cover):
     """
     basic graph renderer / without vertex cover
-    :param adjency_matrix: adjency matrix of the graph
+    :param adjacency_matrix: adjency matrix of the graph
     :param vertex_cover: vertex cover of the graph as a list of nodes
     :return:
     """
     graph = nx.Graph()
-    for i in range(len(adjency_matrix)):
+    for i in range(len(adjacency_matrix)):
         graph.add_node(i)
-        for j in range(len(adjency_matrix)):
-            if adjency_matrix[i][j] == 1:
+        for j in range(len(adjacency_matrix)):
+            if adjacency_matrix[i][j] == 1:
                 graph.add_edge(i, j)
 
     start_pos = nx.spring_layout(graph, seed=100)
@@ -47,8 +45,8 @@ def vertex_cover_graph(adjency_matrix, vertex_cover):
     nx.draw_networkx_nodes(graph, start_pos, nodelist=vertex_cover, node_color='r', node_size=500)
     plt.show()
 
-# adjency_matrix = load_instance("instances/5_0.2_0.2.txt")
-# vertex_cover_graph(adjency_matrix, [4, 7, 10])
+# adjacency_matrix = load_instance("instances/5_0.2_0.2.txt")
+# vertex_cover_graph(adjacency_matrix, [4, 7, 10])
 
 
 # 3. multiple vertex cover renderer
