b74a95d07ef0c7d7cf75d309a1da518c63051702
Daniel Zelenak <daniel.zelenak@rwth-aachen.de>
2023-07-10 15:54:32 +0200

    using black/white coloring for scientific paper representation

--- instance_renderer.py
+++ instance_renderer.py
@@ -22,28 +22,31 @@ def draw_graph(graph, vertex_cover=None):
     Draw a NetworkX graph with optional vertex cover.
     :param graph: The graph to be drawn
     :param vertex_cover: vertex cover of the graph as a list of nodes (optional)
+    :param scale_factor: The factor by which to scale the positions of the nodes
     :return:
     """
+    # square figure
+    plt.figure(figsize=(10, 10))
+
     # Use a circular layout for the nodes
     pos = nx.circular_layout(graph)
 
     # Set node and edge drawing options
-    node_options = {"node_size": 500, "alpha": 0.8}
+    node_options = {"node_size": 500, "alpha": 1.0}  # Alpha set to 1.0 for non-transparent nodes
     edge_options = {"width": 2, "alpha": 0.3}
 
-    # Draw non-vertex-cover nodes in a pastel blue color
+    # Draw non-vertex-cover nodes with a black outline and white fill color, black font
     non_vertex_cover = [node for node in graph.nodes() if vertex_cover is None or node not in vertex_cover]
-    nx.draw_networkx_nodes(graph, pos, nodelist=non_vertex_cover, node_color='lightblue', **node_options)
+    nx.draw_networkx_nodes(graph, pos, nodelist=non_vertex_cover, edgecolors='black', node_color='white', **node_options)
+    nx.draw_networkx_labels(graph, pos, labels={node: node for node in non_vertex_cover}, font_color='black')
 
     if vertex_cover is not None:
-        # Draw vertex-cover nodes in a pastel red color
-        nx.draw_networkx_nodes(graph, pos, nodelist=vertex_cover, node_color='salmon', **node_options)
+        # Draw vertex-cover nodes with a white outline and black fill color, white font
+        nx.draw_networkx_nodes(graph, pos, nodelist=vertex_cover, edgecolors='white', node_color='black', **node_options)
+        nx.draw_networkx_labels(graph, pos, labels={node: node for node in vertex_cover}, font_color='white')
 
     # Draw edges
     nx.draw_networkx_edges(graph, pos, **edge_options)
-
-    # Draw node labels
-    nx.draw_networkx_labels(graph, pos)
 
     plt.show()
 
@@ -79,7 +82,9 @@ def vertex_cover_graph(adjacency_matrix, vertex_cover):
 if __name__ == "__main__":
     # Testing the renderer methods
     from instance_generator import load_instance
+    from mvc_solver import ilp_solve_mvc
 
-    adjacency_matrix = load_instance("instances/50_0.2_0.18.txt")
+    adjacency_matrix = load_instance("instances/15_0.2_0.2.txt")
     plain_graph(adjacency_matrix)
-    vertex_cover_graph(adjacency_matrix, [0, 1, 2, 3, 4, 5, 6, 7, 8])
+    mvc_optimum = ilp_solve_mvc(adjacency_matrix)
+    vertex_cover_graph(adjacency_matrix, mvc_optimum)
