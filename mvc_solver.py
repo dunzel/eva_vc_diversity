e570ab054fcbf731b7cc64b85d74b5ebed4f760c
Daniel Zelenak <daniel.zelenak@rwth-aachen.de>
2023-07-11 10:33:24 +0200

    added comments to mvc solver file

--- mvc_solver.py
+++ mvc_solver.py
@@ -2,11 +2,17 @@ from gurobipy import *
 
 
 def ilp_solve_mvc(adjacency_matrix):
-    # convert adjacency matrix to graph
+    """
+    Solves the minimum vertex cover problem using an ILP formulation
+    :param adjacency_matrix: the adjacency matrix of the graph
+    :return: the minimum vertex cover
+    """
+
+    # Convert adjacency matrix to graph
     V = list(range(len(adjacency_matrix)))
     E = []
 
-    # consider that the graph is undirected, so we only need to consider the upper triangle
+    # Consider that the graph is undirected, so we only need to consider the upper triangle
     for i in range(len(V)):
         for j in range(i + 1, len(V)):
             if adjacency_matrix[i][j]:
@@ -22,14 +28,15 @@ def ilp_solve_mvc(adjacency_matrix):
 
     model.update()
 
-    # objective function
+    # Objective function
     model.setObjective(quicksum(x[v] for v in V), GRB.MINIMIZE)
 
-    # constraints
+    # Constraints
     for e in E:
         model.addConstr(x[e[0]] + x[e[1]] >= 1)
 
-    # solve the model
+    # Solve the model
     model.optimize()
 
+    # Return the solution as a list of vertex indices
     return [v for v in V if x[v].x == 1]
