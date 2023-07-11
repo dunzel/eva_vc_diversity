bb4293a82c20d12e980d40a8b350461fbb6c42cb
Daniel Zelenak <daniel.zelenak@rwth-aachen.de>
2023-07-11 10:03:17 +0200

    added comments to fitness_functions.py

--- eva_algos/fitness_functions.py
+++ eva_algos/fitness_functions.py
@@ -1,11 +1,61 @@
 import numpy as np
 
-# ToDo: diversity function
-def diversity_func(population):
-    pass
+##############################
+# New fitness function ideas #
+##############################
+
+# function head needs to be: def fitness_func(ind, population):
+# ind is the individual for which the fitness is calculated
+# population is the whole population
 
 
-# ToDo: fitness function for unconstrained case
+def mvc_optimal_fitness(ind, population):
+    """
+    An optimal fitness function for the MVC problem
+    an individual is fitter the smaller the vertex cover is
+    :param ind: binary list
+    :param population: list of binary lists
+    :return: number of vertices that are NOT in the vertex cover
+    """
+    fitness = len(ind) - np.sum(ind)
+    return fitness
+
+
+def hamming_distance(ind1, ind2):
+    """
+    Calculates the hamming distance between two individuals
+    :param ind1: binary list
+    :param ind2: binary list
+    :return:
+    """
+    return sum(el1 != el2 for el1, el2 in zip(ind1, ind2))
+
+
+def mvc_hamming_diversity(ind, population):
+    """
+    Calculates the hamming diversity of all pairs of individuals in the population without the individual ind
+    :param ind: binary list
+    :param population: list of binary lists
+    :return: summed hamming diversity
+    """
+
+    # Copy the population and remove the individual from the population
+    population = [i for i in population if i != ind]
+
+    # calculate the diversity of the remaining population
+    diversity = 0
+    for i in population:
+        for j in population:
+            if i != j:
+                diversity += hamming_distance(i, j)
+
+    return diversity
+
+##############################
+# Old fitness function ideas #
+##############################
+
+
 def fitness_func(ga_instance, solution, solution_idx, population):
     fitness = len(ga_instance.population) * len(solution)
     for individual in ga_instance.population:
@@ -15,7 +65,6 @@ def fitness_func(ga_instance, solution, solution_idx, population):
     return fitness
 
 
-# ToDo: fitness function for constrained case
 def fitness_func_diversity(ga_instance, solution, solution_idx, population):
     """
     Calculates the diversity by reducing it for every node in the solutions vertex cover
@@ -31,34 +80,3 @@ def fitness_func_diversity(ga_instance, solution, solution_idx, population):
     print(" for ", end="")
     print(solution)
     return fitness
-
-
-def mvc_optimal_fitness(ind, population):
-    fitness = len(ind) - np.sum(ind)
-    return fitness
-
-
-def node_overlap(ind1, ind2):
-    # not a good idea:
-    # tends to set every individual to list of 0s
-    VC_set_1 = set(i for i, x in enumerate(ind1) if x == 1)
-    VC_set_2 = set(i for i, x in enumerate(ind2) if x == 1)
-    return len(VC_set_1 & VC_set_2)
-
-
-def hamming_distance(ind1, ind2):
-    return sum(el1 != el2 for el1, el2 in zip(ind1, ind2))
-
-
-def mvc_hamming_diversity(ind, population):
-    # Copy the population and remove the individual from the population
-    population = [i for i in population if i != ind]
-
-    # calculate the diversity of the remaining population
-    diversity = 0
-    for i in population:
-        for j in population:
-            if i != j:
-                diversity += hamming_distance(i, j)
-
-    return diversity
