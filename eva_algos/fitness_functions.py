ec51d5e152457a7cdbdd4ce1530fdd295addacd6
Daniel Zelenak <daniel.zelenak@rwth-aachen.de>
2023-07-12 18:15:22 +0200

    removed unused pygad code and added numpy to requirements.txt

--- eva_algos/fitness_functions.py
+++ eva_algos/fitness_functions.py
@@ -90,33 +90,3 @@ def mvc_hamming_diversity_cmp(ind, population):
             diversity += hamming_distance(ind, i)
 
     return diversity
-
-##############################
-# Old fitness function ideas #
-##############################
-
-
-def fitness_func(ga_instance, solution, solution_idx, population):
-    fitness = len(ga_instance.population) * len(solution)
-    for individual in ga_instance.population:
-        for i in range(len(solution)):
-            if solution[i] != individual[i]:
-                fitness = fitness + 1
-    return fitness
-
-
-def fitness_func_diversity(ga_instance, solution, solution_idx, population):
-    """
-    Calculates the diversity by reducing it for every node in the solutions vertex cover
-    """
-    fitness = len(ga_instance.population) * len(solution)
-    for individual in ga_instance.population:
-        for i in range(len(solution)):
-            if solution[i] == 1:
-                if individual[i] == 1:
-                    fitness = fitness - 1
-    print("fitness: ", end="")
-    print(fitness, end="")
-    print(" for ", end="")
-    print(solution)
-    return fitness
