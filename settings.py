0f2b7b01983f4e09cd658aeddd9d389f9f9243cf
Daniel Zelenak <daniel.zelenak@rwth-aachen.de>
2023-07-12 16:47:49 +0200

    changed some settings and add LOGGING setting

--- settings.py
+++ settings.py
@@ -5,19 +5,20 @@ from eva_algos.initial_population import all_ones_pop, all_mvc_pop, heuristic_po
 from instances.instance_generator import load_instance
 from eva_algos.operators import multi_node_swap
 
-GRAPH_FILE_NAME = "instances/5_0_0.0.txt"
+GRAPH_FILE_NAME = "instances/100_8.txt"
 GRAPH_INSTANCE = load_instance(GRAPH_FILE_NAME)
 NUM_GENES = len(GRAPH_INSTANCE)  # Don't change this
 
-MU = 32
+MU = 2
 NUM_GENERATIONS = MU * 2 * NUM_GENES
 EARLY_DIVERSE_STOP = False      # if True, the algorithm will stop if all individuals in the population are different
 EARLY_DIVERSE_STOP_CNT = 0      # if True, and the diversity has not increased for this many generations, the algorithm will stop
 NO_FIT_IMP_STOP_CNT = 100       # if True, and the fitness has not increased for this many generations, the algorithm will stop
-CONSTRAINED = False              # if True, the algorithm is constrained and will use OPT as an upper bound
+CONSTRAINED = False             # if True, the algorithm is constrained and will use OPT as an upper bound
 ALPHA = 0.05
 RANDOM_SEED = 42
 DEBUG = False
+LOGGING = False
 
 # Callbacks
 POPULATION_GENERATOR = all_ones_pop
